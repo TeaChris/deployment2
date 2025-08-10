de646a7831152d3efd1fdb5e07c237eae49b9ab5
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-10 13:54:44 +0100

    feat(auth): add sign-up page with form validation
    Implement sign-up page with form fields for username, email, password and confirm password. Includes validation using zod schema and error handling. Provides link to sign-in page for existing users.

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import { signupSchema, TSignUpSchema, cn } from '@/utils'
+import { Button, buttonVariants, Input, Label } from '@/components'
+
+import Link from 'next/link'
+
+import { toast } from 'sonner'
+import { useForm } from 'react-hook-form'
+import { ArrowRight, Loader2 } from 'lucide-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+export default function Page() {
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TSignUpSchema>({
+    resolver: zodResolver(signupSchema),
+  })
+
+  const onSubmit = (values: TSignUpSchema) => {
+    reset()
+  }
+
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <div className="text-2xl font-extrabold text-primary">Flash.</div>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Signup to flash.
+            </h1>
+            <Link
+              className={buttonVariants({
+                variant: 'ghost',
+                className: 'gap-1.5 text-black',
+              })}
+              href="/sign-in"
+            >
+              Already have an account? Sign-in
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+
+          <div className="grid gap-6">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="username">Username</Label>
+                  <Input
+                    {...register('username')}
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.username,
+                    })}
+                    placeholder="User"
+                    // disabled={isPending}
+                  />
+                  {errors?.username && (
+                    <p className="text-sm text-red-500">
+                      {errors.username.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    {...register('email')}
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.email,
+                    })}
+                    placeholder="you@example.com"
+                    // disabled={isPending}
+                  />
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    {...register('password')}
+                    type="password"
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.password,
+                    })}
+                    placeholder="Password"
+                    // disabled={isPending}
+                  />
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Confirm Password</Label>
+                  <Input
+                    {...register('confirmPassword')}
+                    type="password"
+                    className={cn({
+                      'focus-visible:ring-red-500': errors.confirmPassword,
+                    })}
+                    placeholder="Password"
+                    // disabled={isPending}
+                  />
+                  {errors?.confirmPassword && (
+                    <p className="text-sm text-red-500">
+                      {errors.confirmPassword.message}
+                    </p>
+                  )}
+                </div>
+                <Button>sign up</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
