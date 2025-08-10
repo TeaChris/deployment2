afc3477c60a794b7e9ce6d7d56f4907208921d8e
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-10 13:55:42 +0100

    feat(components): add index file for component exports
    Create a central export file for components to simplify imports across the application

---
+++ src/components/index.ts
@@ -0,0 +1,16 @@
+/*
+ * ############################################################################### *
+ * Created Date: Su Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sun Aug 10 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+export * from './icons'
+export * from './navbar'
+
+export * from './ui'
