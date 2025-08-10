c79dd4a3598d35d84de5b92ed6c062514b08f80c
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-10 13:57:48 +0100

    feat(utils): add cn utility function for class merging
    Add a utility function that combines clsx and twMerge to handle class name merging with Tailwind CSS

---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+/*
+ * ############################################################################### *
+ * Created Date: Su Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Sun Aug 10 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                     *
+ * ############################################################################### *
+ */
+
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
