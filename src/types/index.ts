a61ed071b7f7bb15eeb0c05291cf780fbc192513
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-11 06:08:33 +0100

    docs(types): add index file for type exports
    Add centralized export file for types to simplify imports across the codebase

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+/*
+ * ############################################################################### *
+ * Created Date: Mo Aug 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Mon Aug 11 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                               *
+ * ############################################################################### *
+ */
+
+export * from './app'
