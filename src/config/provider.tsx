a22905741332a4044c8629b14d1bb81f50ff796d
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-12 10:56:36 +0100

    feat(provider): wrap QueryClientProvider in HydrationWrapper
    Integrate the HydrationWrapper component around the QueryClientProvider to ensure proper hydration handling in Next.js applications. This change enhances the rendering process and maintains consistency with client-side rendering practices.

--- src/config/provider.tsx
+++ src/config/provider.tsx
@@ -3,18 +3,32 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { ReactNode, useState } from 'react'
+import HydrationWrapper from './hydration'
 
 type Props = {
   children: ReactNode
 }
 
 export default function Providers({ children }: Props) {
+  // const [queryClient] = useState(
+  //   () =>
+  //     new QueryClient({
+  //       defaultOptions: {
+  //         queries: {
+  //           staleTime: 60 * 1000, // 1 minute
+  //         },
+  //       },
+  //     })
+  // )
+
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <Toaster duration={8000} position={'top-center'} richColors />
-    </QueryClientProvider>
+    <HydrationWrapper>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <Toaster duration={8000} position={'top-center'} richColors />
+      </QueryClientProvider>
+    </HydrationWrapper>
   )
 }
