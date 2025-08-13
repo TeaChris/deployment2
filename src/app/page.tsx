da953cabaab2d617908152dfbc9932980e79a0b0
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-13 06:10:40 +0100

    refactor(page): remove auth-related code and simplify home page
    Remove unused auth store imports and related state management logic. Simplify the home page to display a static title instead of dynamic user email.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
 'use client'
 
-import { useAuthStore } from '@/store'
-import { useEffect, useState } from 'react'
-
 export default function Home() {
-  const { user } = useAuthStore()
-
-  const [isMounted, setIsMounted] = useState<boolean>(false)
-
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-
-  if (!isMounted) {
-    return null
-  }
-
-  console.log({ user: user?.email })
-  return <h1 className="text-7xl font-bold">{user?.email}</h1>
+  return <h1 className="text-7xl font-bold">Page</h1>
 }
