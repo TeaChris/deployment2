38e4df99ac432bc3c1a19ad2c087972a25f7cd65
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-08-14 19:06:16 +0100

    feat(auth): add auth store import and useEffect hook
    Initialize auth state management by importing useAuthStore and adding useEffect hook for potential side effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { useAuthStore } from '@/store'
+import { useEffect } from 'react'
+
 export default function Home() {
   return <h1 className="text-7xl font-bold">Page</h1>
 }
